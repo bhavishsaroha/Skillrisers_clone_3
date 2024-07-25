bbcd4a73560c5e480abc469eb9804d511791ed93
bhavishsaroha <157137135+bhavishsaroha@users.noreply.github.com>
2024-07-26 03:19:26 +0530

    Update vite.config.js

--- frontend3/vite.config.js
+++ frontend3/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // This should be the directory where Vite outputs build files
+  },
   plugins: [react()],
 })
